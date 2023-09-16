b34e3f31ac67176aac5f606e0f2a363730ffca3a
Yuriy Sorochinskiy <yuriy.ys314@mail.ru>
2023-09-16 19:44:29 +0600

    channel

--- src/channel.py
+++ src/channel.py
@@ -1,11 +1,15 @@
+import json
+import os
 
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
