c7d1da10d016f13010fb46987f9d591c6345fad7
Yuriy Sorochinskiy <yuriy.ys314@mail.ru>
2023-09-21 15:07:02 +0600

    print_info

--- src/channel.py
+++ src/channel.py
@@ -4,6 +4,12 @@ import os
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
 
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+
+# создать специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
+
 class Channel:
     """Класс для ютуб-канала"""
 
@@ -12,5 +18,11 @@ class Channel:
         self.channel_id = channel_id
 
     def print_info(self) -> None:
-        """Выводит в консоль информацию о канале."""
-        pass
+        """Выводит в консоль информацию о канале.
+        Выводит словарь в json-подобном удобном формате с отступами"""
+
+        # channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+        channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+        channel = youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
+
