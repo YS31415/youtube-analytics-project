1c4be0ce67d26b8add25aac3889ad2cca7d77561
Yuriy Sorochinskiy <yuriy.ys314@mail.ru>
2023-09-28 17:51:27 +0600

    homework-2

--- src/channel.py
+++ src/channel.py
@@ -10,9 +10,11 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        self.__channel_id = channel_id
         # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
         self.__api_key = os.getenv('YT_API_KEY')
+        self.__title = None
+        self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
 
         # создать специальный объект для работы с API
         self.__youtube = build('youtube', 'v3', developerKey=self.__api_key)
@@ -26,3 +28,26 @@ class Channel:
         channel = self.__youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
 
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @property
+    def title(self):
+        return self.__youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()["items"][0]['snippet']['title']
+
+    @property
+    def video_count(self):
+        channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+        return self.__youtube.channels().list(id=channel_id, part='snippet,statistics').execute()["items"][0]["statistics"]['videoCount']
+
+    @classmethod
+    def get_service(cls):
+        return build('youtube', 'v3', developerKey=os.getenv('YT_API_KEY'))
+
+    def to_json(self, path):
+        channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+        # channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
+        channel = self.__youtube.channels().list(id=channel_id, part='snippet,statistics').execute()
+        with open(path, 'w') as f:
+            f.write(json.dumps(channel, indent=2, ensure_ascii=False))
