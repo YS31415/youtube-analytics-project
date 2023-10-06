1696598f47670dcb0c85dec174a0bb227bb02d1b
Yuriy Sorochinskiy <yuriy.ys314@mail.ru>
2023-10-06 14:03:40 +0600

    init

--- src/channel.py
+++ src/channel.py
@@ -8,46 +8,45 @@ from googleapiclient.discovery import build
 class Channel:
     """Класс для ютуб-канала"""
 
-    # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
-    api_key = os.getenv('YT_API_KEY')
-
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.__channel_id = channel_id
-
+        self.channel_info = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        self.title = self.channel_info["items"][0]['snippet']['title']
+        self.channel_desc = self.channel_info["items"][0]['snippet']["description"]
         self.url = f'https://www.youtube.com/channel/{self.__channel_id}'
-
+        self.subs_coutn = self.channel_info["items"][0]["statistics"]["subscriberCount"]
+        self.video_count = self.channel_info["items"][0]["statistics"]['videoCount']
+        self.channel_vives = self.channel_info["items"][0]["statistics"]["viewCount"]
         # создать специальный объект для работы с API
-        self.__title = Channel.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()["items"][0]['snippet']['title']
-        self.__video_count = Channel.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()["items"][0]["statistics"]['videoCount']
+
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале.
         Выводит словарь в json-подобном удобном формате с отступами"""
 
-        channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
         #channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
-        channel = Channel.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
+        channel = self.get_service().channels().list(id=self.__channel_id, part='snippet,statistics').execute()
         print(json.dumps(channel, indent=2, ensure_ascii=False))
 
     @property
     def channel_id(self):
         return self.__channel_id
 
-    @property
-    def title(self):
-        return self.__title
-
-    @property
-    def video_count(self):
-        return self.__video_count
-
     @classmethod
     def get_service(cls):
-        return build('youtube', 'v3', developerKey=cls.api_key)
+        # создать специальный объект для работы с API
+        api_key = os.getenv('YT_API_KEY')
+        # YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+        return build('youtube', 'v3', developerKey=api_key)
 
-    def to_json(self, path):
-        channel_id = 'UC-OVMPlMA3-YCIeg4z5z23A'  # MoscowPython
+    def to_json(self, file_name):
         # channel_id = 'UCwHL6WHUarjGfUM_586me8w'  # HighLoad Channel
-        channel = Channel.get_service().channels().list(id=channel_id, part='snippet,statistics').execute()
-        with open(path, 'w') as f:
-            f.write(json.dumps(channel, indent=2, ensure_ascii=False))
+        data = {'channel_id': self.channel_id,
+                'channel_title': self.title,
+                'channel_desc': self.channel_desc,
+                'url': self.url,
+                'subs_coutn': self.subs_coutn,
+                'video_count': self.video_count,
+                'channel_vives': self.channel_vives}
+        with open(file_name, 'w', encoding='UTF-8') as f:
+            json.dump(data, f, indent=2, ensure_ascii=False)
